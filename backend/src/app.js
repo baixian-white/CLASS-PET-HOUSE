fa04f4b3364f2d346a31ac25d3ed87f3f0f22d37
Alvin_white <1503948873@qq.com>
2026-03-21 16:52:32 +0800

    修复代码错误

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     console.log(`[IP DEBUG] Direct RemoteIP: ${req.socket?.remoteAddress}`);
     console.log("-----------------------------------------");
   }
-  next();以
+  next();
 });
 // ============================================
 
